04480c82dd807784ee32189c4707ff551c34289e
Roman Handke <roman.handke@online.de>
2020-03-29 19:52:39 +0200

    Create Education Services

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    migrationServices: []
+    migrationServices: [],
+    educationServices: []
   },
   mutations: {
     SET_MIGRATION_SERVICES(state, services) {
       state.migrationServices = services;
+    },
+    SET_EDUCATION_SERVICES(state, services) {
+      state.educationServices = services;
     }
   },
   actions: {
@@ -22,6 +26,17 @@ export default new Vuex.Store({
         .then(response => {
           let posts = response.data;
           commit("SET_MIGRATION_SERVICES", posts);
+        })
+        .catch(error => console.log(error));
+    },
+    loadEducationServices({ commit }) {
+      axios
+        .get(
+          "https://my-json-server.typicode.com/romanhandke/json-placeholder/services"
+        )
+        .then(response => {
+          let posts = response.data;
+          commit("SET_EDUCATION_SERVICES", posts);
         })
         .catch(error => console.log(error));
     }
--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="education">
     <banner v-bind:banner="banner"></banner>
+    <div class="container">
+      <service-list v-bind:services="educationServices"></service-list>
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from "@/components/Banner";
+import ServiceList from "@/components/ServiceList";
+import { mapState } from "vuex";
 export default {
   name: "Education",
   components: {
-    Banner
+    Banner,
+    ServiceList
   },
   data: () => ({
     banner: {
       title: "Bildung & Erziehung",
       uri: "/assets/images/education_mobile.jpg"
     }
-  })
+  }),
+  mounted() {
+    this.$store.dispatch("loadEducationServices");
+  },
+  computed: {
+    ...mapState(["educationServices"])
+  }
 };
 </script>
 
