3b7ff03e71e187dd86ea2fa9db45f71771c720e2
Roman Handke <roman.handke@online.de>
2020-04-04 17:37:44 +0200

    Refactoring
    Created a component for the footer instead of putting the code directly
    in the App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <v-app>
     <navigation></navigation>
-
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer class="justify-center" color="#292929" height="100">
-      <div
-        class="title font-weight-light grey--text text--lighten-1 text-center"
-      >
-        &copy; {{ new Date().getFullYear() }} — ZIUMA e.V. — Made by Roman
-        Handke
-      </div>
-    </v-footer>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import Navigation from "./components/Navigation";
+import AppFooter from "@/components/Footer";
+
 export default {
   name: "App",
 
   components: {
-    Navigation
+    Navigation,
+    AppFooter
   }
 };
 </script>
