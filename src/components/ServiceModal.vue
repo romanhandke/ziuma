d5a2f80bbb5d870f3004899c5f5f2e5ebc2b6656
Roman Handke <roman.handke@online.de>
2020-04-04 15:35:59 +0200

    Add gallery

--- src/components/ServiceModal.vue
+++ src/components/ServiceModal.vue
@@ -18,6 +18,16 @@
         <v-img :src="service.image"></v-img>
         <v-card-title v-text="service.title"></v-card-title>
         <v-card-text v-text="service.body"></v-card-text>
+        <v-carousel
+          hide-delimiter-background
+          continuous
+          show-arrows
+          v-if="service.slides"
+        >
+          <v-carousel-item v-for="(slide, i) in service.slides" :key="i">
+            <v-img :src="slide" max-width="100%"></v-img>
+          </v-carousel-item>
+        </v-carousel>
       </v-card>
     </v-dialog>
   </v-row>
--- src/views/DiesterwegStipend.vue
+++ src/views/DiesterwegStipend.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="diesterweg-stipend">
     <banner v-bind:banner="banner"></banner>
+    <service-list
+      v-bind:services="dwsData"
+      v-bind:service-name="'dwsData'"
+      v-bind:route="'generation'"
+    ></service-list>
   </div>
 </template>
 
 <script>
 import Banner from "@/components/Banner";
+import ServiceList from "@/components/ServiceList";
+import { mapState } from "vuex";
 export default {
   name: "DiesterwegStipend",
   components: {
-    Banner
+    Banner,
+    ServiceList
   },
   data: () => ({
     banner: {
       title: "Diesterweg- Stipendium",
       uri: "/assets/images/dwstipend_mobile.jpg"
     }
-  })
+  }),
+  mounted() {
+    this.$store.dispatch("loadDwsData");
+  },
+  computed: {
+    ...mapState(["dwsData"])
+  }
 };
 </script>
 
